af92dc536cfa274b9615ac7d662cbba670b7a58c
gulomov <jalolidding98@gmail.com>
2024-10-15 16:49:21 +0200

    Uncomment RecommendationsInHome in HomeScreen

--- home/src/commonMain/kotlin/org/dtcm/work/home/domain/HomeScreen.kt
+++ home/src/commonMain/kotlin/org/dtcm/work/home/domain/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import org.dtcm.work.design.small150
 import org.dtcm.work.home.domain.components.NewsInHome
+import org.dtcm.work.home.domain.components.RecommendationsInHome
 import org.koin.compose.koinInject
 
 @Composable
@@ -33,12 +34,13 @@ fun HomeScreen(
                 onNewsClicked = viewModel::onNewsClicked
             )
         }
-        /*item {
+        item {
             RecommendationsInHome(
                 recommendations = uiState.value.recommendationsList,
-                navController = navController
+                onRecommendationClicked = viewModel::onRecommendationsClicked
             )
         }
+        /*
         item {
             TopProductsLazyRow(
                 productList = uiState.value.topProductsList,
