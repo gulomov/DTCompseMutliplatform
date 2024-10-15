73e10d042f707965338a7df2651755a823af5c2f
gulomov <jalolidding98@gmail.com>
2024-10-15 15:52:14 +0200

    Update getAppDatabase.kt function for android and ios platforms

--- database/src/androidMain/kotlin/org/dtcm/work/database/getAppDatabase.kt
+++ database/src/androidMain/kotlin/org/dtcm/work/database/getAppDatabase.kt
@@ -3,10 +3,13 @@ package org.dtcm.work.database
 import android.content.Context
 import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.coroutines.Dispatchers
 
 fun getAppDatabase(context: Context): AppDatabase {
     val dbFile = context.getDatabasePath("dt-compose.db")
     return Room.databaseBuilder<AppDatabase>(
         context = context.applicationContext, name = dbFile.absolutePath
-    ).setDriver(BundledSQLiteDriver()).fallbackToDestructiveMigration(true).build()
+    ).setDriver(BundledSQLiteDriver())
+        .setQueryCoroutineContext(Dispatchers.IO)
+        .fallbackToDestructiveMigration(true).build()
 }
--- database/src/iosMain/kotlin/org/dtcm/work/database/getAppDatabase.kt
+++ database/src/iosMain/kotlin/org/dtcm/work/database/getAppDatabase.kt
@@ -2,14 +2,31 @@ package org.dtcm.work.database
 
 import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
-import platform.Foundation.NSHomeDirectory
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
 fun getAppDatabase(): AppDatabase {
-    val dbFile = NSHomeDirectory() + "/dt-compose.db"
+    val dbFile = documentDirectory() + "/dt-compose.db"
     return Room.databaseBuilder<AppDatabase>(
         name = dbFile,
-        factory = { AppDatabase::class.instantiateImpl() }
     ).setDriver(BundledSQLiteDriver())
+        .setQueryCoroutineContext(Dispatchers.IO)
         .fallbackToDestructiveMigration(true)
         .build()
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun documentDirectory(): String {
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null,
+    )
+    return requireNotNull(documentDirectory?.path)
 }
