f285a9b35cd03fb3984de83174680d0469d6c6a7
jaloliddingulomov <jaloliddin.gulomov@adastragrp.com>
2024-11-27 16:50:46 +0100

    Format properly

--- domain/src/commonMain/kotlin/org/dtcm/work/domain/GetProductDetailsUseCase.kt
+++ domain/src/commonMain/kotlin/org/dtcm/work/domain/GetProductDetailsUseCase.kt
@@ -4,7 +4,7 @@ import org.dtcm.work.repository.ProductDetailsRepository
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class GetProductDetailsUseCase: KoinComponent {
+class GetProductDetailsUseCase : KoinComponent {
     private val productDetailsRepository: ProductDetailsRepository by inject()
 
     operator fun invoke(productId: String) = productDetailsRepository.getProductDetails(productId)
