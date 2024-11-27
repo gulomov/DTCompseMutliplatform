5a1471b34f0f21dd25565dcc9d659adb4e1cb87f
jaloliddingulomov <jaloliddin.gulomov@adastragrp.com>
2024-11-27 12:26:45 +0100

    Refactor FavoritesViewModel use combining flows technique

--- favorites/src/commonMain/kotlin/org/dtcm/work/favorites/FavoritesViewModel.kt
+++ favorites/src/commonMain/kotlin/org/dtcm/work/favorites/FavoritesViewModel.kt
@@ -4,26 +4,46 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import org.dtcm.work.common.data.data.FavoriteProduct
+import org.dtcm.work.common.data.navigation.ScreenRoute
 import org.dtcm.work.domain.DeleteFromFavoriteProductsUseCase
 import org.dtcm.work.domain.GetFavoriteProductsUseCase
+
+private const val PRODUCT_ID = "{productId}"
 
 class FavoritesViewModel(
     private val getFavoriteProductsUseCase: GetFavoriteProductsUseCase,
     private val deleteFromFavoriteProductsUseCase: DeleteFromFavoriteProductsUseCase
 ) : ViewModel() {
-    private val _favoriteProducts = MutableStateFlow(listOf<FavoriteProduct>())
-    val favoriteProducts = _favoriteProducts.asStateFlow()
+    private val _uiState = MutableStateFlow(FavoritesUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val navigationRoute = MutableStateFlow<String?>(null)
 
     init {
-        getFavoriteItems()
+        viewModelScope.launch {
+            combine(
+                getFavoriteProductsUseCase(),
+                navigationRoute
+            ) { favoriteProducts, navigationRoute ->
+                FavoritesUiState(
+                    favoriteProducts = favoriteProducts,
+                    navigationRoute = navigationRoute
+                )
+            }.collect { newState ->
+                _uiState.value = newState
+            }
+        }
     }
 
-    private fun getFavoriteItems() = viewModelScope.launch {
-        getFavoriteProductsUseCase().collect {
-            _favoriteProducts.value = it
-        }
+    fun onProductClicked(id: Int) {
+        val route = ScreenRoute.PRODUCTION_DETAIL.replace(
+            PRODUCT_ID,
+            id.toString()
+        )
+        navigationRoute.value = route
     }
 
     fun deleteFromFavoriteProducts(productId: Int) = viewModelScope.launch {
