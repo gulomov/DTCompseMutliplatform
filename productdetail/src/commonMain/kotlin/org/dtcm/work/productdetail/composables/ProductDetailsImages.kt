9c3d8589422f1dadab605ab88cb5aa642aa63824
jaloliddingulomov <jaloliddin.gulomov@adastragrp.com>
2024-11-27 17:08:36 +0100

    Move adding to favorite logic to view model

--- productdetail/src/commonMain/kotlin/org/dtcm/work/productdetail/composables/ProductDetailsImages.kt
+++ productdetail/src/commonMain/kotlin/org/dtcm/work/productdetail/composables/ProductDetailsImages.kt
@@ -7,16 +7,12 @@ import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.dtcm.work.common.Res
@@ -24,7 +20,6 @@ import org.dtcm.work.common.ic_bookmark
 import org.dtcm.work.common.ic_bookmark_border
 import org.dtcm.work.database.data.ProductImages
 import org.dtcm.work.design.IndicatorDots
-import org.dtcm.work.design.MainHorizontalPager
 import org.dtcm.work.design.ProductHorizontalPager
 import org.dtcm.work.design.normal175
 import org.dtcm.work.design.small100
@@ -36,25 +31,16 @@ import org.jetbrains.compose.resources.painterResource
 @Composable
 internal fun ProductDetailsImages(
     productImages: List<ProductImages>,
-    saveProductIntoFavoritesClicked: (Boolean) -> Unit,
-    isProductSavedIntoFavorites: Boolean?
+    saveProductIntoFavoritesClicked: () -> Unit,
+    isProductSavedIntoFavorites: Boolean
 ) {
-    val pagerState = rememberPagerState(pageCount = {
-        productImages.size
-    })
+    val pagerState = rememberPagerState(pageCount = { productImages.size })
     val isDragged by pagerState.interactionSource.collectIsDraggedAsState()
-    var isProductSaved by remember { mutableStateOf(isProductSavedIntoFavorites ?: false) }
-    println("isProductSaved: $isProductSaved and isProductSavedIntoFavoritesFlow: $isProductSavedIntoFavorites")
-    LaunchedEffect(isProductSavedIntoFavorites) {
-        isProductSaved = isProductSavedIntoFavorites ?: false
-    }
+
     Box(modifier = Modifier.fillMaxWidth()) {
-        MainHorizontalPager(
-            itemsCount = productImages.size,
-            itemContent = {
-                ProductHorizontalPager(productImages[it].imageUrl)
-            },
-        )
+        HorizontalPager(state = pagerState) {
+            ProductHorizontalPager(productImages[it].imageUrl)
+        }
         Box(
             Modifier
                 .padding(
@@ -65,7 +51,7 @@ internal fun ProductDetailsImages(
         ) {
             Image(
                 painter = painterResource(
-                    if (!isProductSaved) {
+                    if (!isProductSavedIntoFavorites) {
                         Res.drawable.ic_bookmark_border
                     } else {
                         Res.drawable.ic_bookmark
@@ -74,14 +60,12 @@ internal fun ProductDetailsImages(
                 contentDescription = "Favorite",
                 modifier = Modifier
                     .clickable(onClick = {
-                        isProductSaved = isProductSaved == false
-                        saveProductIntoFavoritesClicked(isProductSaved)
+                        saveProductIntoFavoritesClicked()
                     })
             )
         }
         Surface(
-            modifier =
-            Modifier
+            modifier = Modifier
                 .padding(small100)
                 .align(Alignment.BottomCenter),
             shape = CircleShape,
