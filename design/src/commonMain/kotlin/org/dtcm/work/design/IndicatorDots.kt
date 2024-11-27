efdd5fc7f2e51fc9b57c9e47f2a2ea8ac79e3fb9
jaloliddingulomov <jaloliddin.gulomov@adastragrp.com>
2024-11-27 16:50:57 +0100

    Remove MainHorizontalPager

--- design/src/commonMain/kotlin/org/dtcm/work/design/IndicatorDots.kt
+++ design/src/commonMain/kotlin/org/dtcm/work/design/IndicatorDots.kt
@@ -1,42 +1,17 @@
 package org.dtcm.work.design
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun MainHorizontalPager(
-    itemContent: @Composable (index: Int) -> Unit,
-    itemsCount: Int,
-    modifier: Modifier = Modifier
-) {
-    HorizontalPager(
-        state = rememberPagerState { itemsCount },
-        modifier = modifier.fillMaxWidth()
-    ) { page ->
-        key(page) {
-            itemContent(page)
-        }
-    }
-}
 
 @Composable
 fun IndicatorDots(
