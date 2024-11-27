66009f32575b22288187fa02364633b43f7c5cd6
jaloliddingulomov <jaloliddin.gulomov@adastragrp.com>
2024-11-27 11:14:18 +0100

    Add structure more

--- common/src/commonMain/kotlin/org/dtcm/work/common/data/data/AllProductsList.kt
+++ common/src/commonMain/kotlin/org/dtcm/work/common/data/data/AllProductsList.kt
@@ -8,19 +8,20 @@ import org.dtcm.work.database.data.ProductSizes
 data class AllProductsList(
     val allProductsList: List<AllProductsItem>? = emptyList(),
 )
+
 @Serializable
 data class AllProductsItem(
     val address: String? = null,
     val id: Int? = null,
-    val images: List<ProductImages>? = null,
-    val title: String? = null,
     val salePercentage: Int? = null,
-    val saleStartsDate: String? = null,
-    val saleEndsDate: String? = null,
     val originalPrice: Int? = null,
+    val title: String? = null,
     val priceOnSale: Int? = null,
+    val saleEndsDate: String? = null,
+    val saleStartsDate: String? = null,
+    val brand: String? = null,
+    val images: List<ProductImages>? = null,
     val sizes: List<ProductSizes>? = null,
-    val brand: String? = null
 ) {
     fun asFavoriteProduct() = FavoriteProduct(
         address = address,
