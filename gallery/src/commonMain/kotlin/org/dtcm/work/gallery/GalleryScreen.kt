490e961776d991e4cf83d0ef894877c20b744e6c
jaloliddingulomov <jaloliddin.gulomov@adastragrp.com>
2024-11-27 11:15:50 +0100

    Remove extra space and not necessary LaunchEffect

--- gallery/src/commonMain/kotlin/org/dtcm/work/gallery/GalleryScreen.kt
+++ gallery/src/commonMain/kotlin/org/dtcm/work/gallery/GalleryScreen.kt
@@ -1,16 +1,11 @@
 package org.dtcm.work.gallery
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import org.dtcm.work.design.normal100
 import org.dtcm.work.gallery.components.BrandsInGallery
 import org.dtcm.work.gallery.components.ProductsInGallery
 import org.koin.compose.koinInject
@@ -21,25 +16,19 @@ fun GalleryScreen(
     modifier: Modifier = Modifier,
     viewModel: GalleryScreenViewModel = koinInject()
 ) {
-
     val uiState = viewModel.uiState.collectAsState()
-
     uiState.value.navigationRoute?.let {
         navController.navigate(it)
-    }
-
-    LaunchedEffect(Unit) {
-        viewModel.getFavoriteProductsIds()
     }
 
     Column(
         horizontalAlignment = Alignment.Start,
         modifier = modifier,
     ) {
-        BrandsInGallery(uiState.value.brandsList, brandClick = {
-            viewModel.loadProductsByBrands(brandName = it)
-        })
-        Spacer(modifier = Modifier.height(normal100))
+        BrandsInGallery(
+            uiState.value.brandsList,
+            onBrandClicked = viewModel::loadProductsByBrands
+        )
         ProductsInGallery(
             brands = uiState.value.brandsList,
             products = uiState.value.productsList,
