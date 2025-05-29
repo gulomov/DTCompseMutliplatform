39d739bfb422fb60cd7024c19779f855d69036de
jaloliddingulomov <jaloliddin.gulomov@adastragrp.com>
2025-05-29 11:49:37 +0200

    feat: add fix me

--- favorites/src/commonMain/kotlin/org/dtcm/work/favorites/FavoritesUiState.kt
+++ favorites/src/commonMain/kotlin/org/dtcm/work/favorites/FavoritesUiState.kt
@@ -2,6 +2,8 @@ package org.dtcm.work.favorites
 
 import org.dtcm.work.common.data.data.FavoriteProduct
 
+// FIXME: Change data class into sealed class
+
 data class FavoritesUiState(
     val favoriteProducts: List<FavoriteProduct> = emptyList(),
     val navigationRoute: String? = null
